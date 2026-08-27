117c6f8726116001ee817845ab5f976c9ccffce4
Kevin <kaharakevin254@gmail.com>
2026-08-27 17:12:24 +0300

    Update player scores when they win a round

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Rock-paper-Scissors-game
+
+    GAME OVERVIEW
+RPS (Rock,Paper,Scissors) is a two party game whereby the parties involved 
+choose one of the three options i.e either Rock, Paper or Scissors.
+
+-Rock is signified with a closed fist
+-Paper is signified with a fully opened palm
+-Scissors is signified with the middle and index finger fully portrayed out like a real scissor
+
+    GAME LOGIC
+-Rock crushes Scissors
+-Scissors cuts Paper
+-Paper smothers Rock
+
+    HOW IT IS PLAYED
+1. There has to be two parties involved.
+2. The parties have to choose either Rock or Paper or Scissors
+3. Before either party throws their choice they have to count to 3 and simultaneously and immediately throw their choice
+4.The one with the superior choice Wins the round
---
+++ app.js
@@ -0,0 +1,115 @@
+/**
+ * User makes a move ( One of 3 options i.e Rock || Paper || Scissors )
+ * Computer makes a move ( One of 3 options i.e Rock || Paper || Scissors)
+ * Winner for the round is displayed 
+ * Five rounds are played
+ * The one with the most wins in the 5 rounds is the Winner
+ * Start over after the 5 rounds if you wish to play again
+ */
+
+/**
+ *     WHAT IS NEEDED
+ * 
+ * User input choice of the 3 options - Modal input
+ * Computer to make a choice of the 3 options randomly - Computer takes a random number and displays the string value of the option
+ * We need a score board which is increased when either party wins
+ * Implementation of the game logic for one time play (One round)
+ * We need to play 5 times inorder to get the overall winner
+ */
+
+const playerInput = prompt("Enter a Choice!");
+const computerChoice = Math.floor(Math.random() * 3);
+const playerMove = getPlayerChoice();
+const computerMove = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+
+    if(computerChoice === 1) {
+        return 'rock';
+    } else if(computerChoice === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function getPlayerChoice() {
+    if(playerInput === 'rock') {
+        return 'rock';
+    } else if(playerInput === 'paper') {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function playRound(playerInput, computerChoice) {
+
+    if(playerInput === 'rock') {
+
+        if(computerChoice === 'rock') {
+            return 'It is a tie!';
+        } 
+        else if(computerChoice === 'paper') {
+            computerScore++;
+            return 'Computer wins this round!';
+        } 
+        else if(computerChoice === 'scissors') {
+            playerScore++;
+            return 'Player wins this round!';
+        }
+
+    }
+
+    if(playerInput === 'paper') {
+
+        if(computerChoice === 'rock') {
+            playerScore++;
+            return 'Player wins this round!';
+        } 
+        else if(computerChoice === 'paper') {
+            return 'It is a tie!';
+        } 
+        else if(computerChoice === 'scissors') {
+            computerScore++;
+            return 'Computer wins this round!'
+        }
+
+    }
+
+    if(playerInput === 'scissors') {
+
+        if(computerChoice === 'rock') {
+            computerScore++;
+            return 'Computer wins this round!';
+        } 
+        else if(computerChoice === 'paper') {
+            playerScore++;
+            return 'Player wins this round!';
+        } 
+        else if(computerChoice === 'scissors') {
+            return 'It is a tie!'
+        }
+
+    }
+
+}
+
+function playGame() {
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+}
+
+
+console.log('Player chose: ', playerMove); 
+console.log('Computer chose: ', computerMove);
+console.log('Player-Score: ', playerScore);
+console.log('Computer-Score: ', computerScore);
+console.log('Who wins : ', playRound(playerMove, computerMove));
